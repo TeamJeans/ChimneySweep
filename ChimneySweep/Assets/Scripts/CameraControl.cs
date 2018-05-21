acdba4fed853ad2307a0cb59273cc28fad3a7455
AidanRMurray <AidanRMurray@users.noreply.github.com>
2018-05-21 17:53:17 +0100

    Started to code the camera control script

---
+++ ChimneySweep/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		// Camera is set to look at the top tile
+
+		// If the player left clicks/touches the screen and moves the mouse/their finger, 
+	}
+}
