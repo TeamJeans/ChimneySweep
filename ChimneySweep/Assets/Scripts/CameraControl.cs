20e6b6805b7f85c0a163d69af14023b3c7fa0b8e
AidanRMurray <AidanRMurray@users.noreply.github.com>
2018-05-22 22:44:42 +0100

    Some code

--- ChimneySweep/Assets/Scripts/CameraControl.cs
+++ ChimneySweep/Assets/Scripts/CameraControl.cs
@@ -1,17 +1,28 @@
-﻿using UnityEngine;
+using UnityEngine;
 
 public class CameraControl : MonoBehaviour {
 
+	Transform mouseOrigin;
+	Transform mouseNewPos;
+
+	bool leftMouseDown;
+
 	// Use this for initialization
 	void Start () {
-		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
 		// Camera is set to look at the top tile
 
-		// If the player left clicks/touches the screen and moves the mouse/their finger, 
+		// If the player left clicks/touches the screen and moves the mouse/their finger, the camera will move in the opposite direction of the drag
+
+		leftMouseDown = Input.GetKey(KeyCode.Mouse0);
+		if (leftMouseDown)
+		{
+			Debug.Log("Mouse0 has been pressed!");
+
+		}
 	}
 }
