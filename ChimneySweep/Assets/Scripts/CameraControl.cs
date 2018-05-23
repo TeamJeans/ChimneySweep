66e26b066bcccdad43ac1a313e215e0e9f9ae213
AidanRMurray <AidanRMurray@users.noreply.github.com>
2018-05-23 23:12:52 +0100

    More scripting

--- ChimneySweep/Assets/Scripts/CameraControl.cs
+++ ChimneySweep/Assets/Scripts/CameraControl.cs
@@ -2,10 +2,15 @@ using UnityEngine;
 
 public class CameraControl : MonoBehaviour {
 
-	Transform mouseOrigin;
-	Transform mouseNewPos;
+	Vector2 mouseOrigin;
+	Vector2 mouseNewPos;
 
-	bool leftMouseDown;
+	bool leftMouseDown = false;
+	bool dragOriginSet = false;
+
+	float elapsedDragTime = 0f;
+	float dragDistance = 0f;
+	float dragSpeed = 0f;
 
 	// Use this for initialization
 	void Start () {
@@ -21,8 +26,20 @@ public class CameraControl : MonoBehaviour {
 		leftMouseDown = Input.GetKey(KeyCode.Mouse0);
 		if (leftMouseDown)
 		{
-			Debug.Log("Mouse0 has been pressed!");
-
+			if (!dragOriginSet)
+			{
+				dragOriginSet = true;
+				mouseOrigin = Input.mousePosition;
+			}
+			mouseNewPos = Input.mousePosition;
+			elapsedDragTime += Time.deltaTime;
+			dragDistance = mouseNewPos.y - mouseOrigin.y;
+			dragSpeed = dragDistance / elapsedDragTime;
+			transform.position.Set(transform.position.x, transform.position.y + dragSpeed, transform.position.z);
+		}
+		else
+		{
+			dragOriginSet = false;
 		}
 	}
 }
