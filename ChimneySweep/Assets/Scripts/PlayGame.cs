cd36346448271777dac43d84da1829538040219c
JackChateauLoney <32193286+JackChateauLoney@users.noreply.github.com>
2018-05-26 15:24:36 +0100

    Added main menu and empty options menu

---
+++ ChimneySweep/Assets/Scripts/PlayGame.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayGame : MonoBehaviour {
+
+    //goes to next scene in build queue
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
