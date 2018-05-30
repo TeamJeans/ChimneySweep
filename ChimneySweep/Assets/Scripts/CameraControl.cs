2ca43948e04aef5d333a2f9ab2e77e3c4375ce2a
AidanRMurray <AidanRMurray@users.noreply.github.com>
2018-05-30 20:48:44 +0100

    Progress towards swiping controls

--- ChimneySweep/Assets/Scripts/CameraControl.cs
+++ ChimneySweep/Assets/Scripts/CameraControl.cs
@@ -1,45 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraControl : MonoBehaviour {
 
-	Vector2 mouseOrigin;
-	Vector2 mouseNewPos;
+	public Swipe swipeControls;
+	private Vector3 desiredPosition;
 
-	bool leftMouseDown = false;
-	bool dragOriginSet = false;
+	private void Update()
+	{
+		if (swipeControls.SwipeDown)
+			desiredPosition += Vector3.up;
 
-	float elapsedDragTime = 0f;
-	float dragDistance = 0f;
-	float dragSpeed = 0f;
+		if (swipeControls.SwipeUp)
+			desiredPosition += Vector3.down;
 
-	// Use this for initialization
-	void Start () {
-	}
-	
-	// Update is called once per frame
-	void Update () {
-
-		// Camera is set to look at the top tile
-
-		// If the player left clicks/touches the screen and moves the mouse/their finger, the camera will move in the opposite direction of the drag
-
-		leftMouseDown = Input.GetKey(KeyCode.Mouse0);
-		if (leftMouseDown)
-		{
-			if (!dragOriginSet)
-			{
-				dragOriginSet = true;
-				mouseOrigin = Input.mousePosition;
-			}
-			mouseNewPos = Input.mousePosition;
-			elapsedDragTime += Time.deltaTime;
-			dragDistance = mouseNewPos.y - mouseOrigin.y;
-			dragSpeed = dragDistance / elapsedDragTime;
-			transform.position.Set(transform.position.x, transform.position.y + dragSpeed, transform.position.z);
-		}
-		else
-		{
-			dragOriginSet = false;
-		}
+		transform.position = Vector3.MoveTowards(transform.position, desiredPosition, 100f * Time.deltaTime);
 	}
 }
