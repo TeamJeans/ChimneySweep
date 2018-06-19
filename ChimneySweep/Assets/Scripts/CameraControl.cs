ba86741f2f2cd68ffc03625d2b921f358c108cf6
AidanRMurray <AidanRMurray@users.noreply.github.com>
2018-06-19 20:26:31 +0100

    Made it compatible with android devices

--- ChimneySweep/Assets/Scripts/CameraControl.cs
+++ ChimneySweep/Assets/Scripts/CameraControl.cs
@@ -4,31 +4,31 @@ using UnityEngine;
 
 public class CameraControl : MonoBehaviour {
 
-	Vector2 mousePosOrigin;
-	Vector2 mouseCurrentPos;
-	Vector2 dragDisplacement;
+	//Vector2 mousePosOrigin;
+	//Vector2 mouseCurrentPos;
+	//Vector2 dragDisplacement;
 
-	void Update()
-	{
-		if (Input.GetMouseButtonDown(0))
-		{
-			mousePosOrigin = Input.mousePosition;
-			Debug.Log("Origin point for mouse set");
-		}
+	//void Update()
+	//{
+	//	if (Input.GetMouseButtonDown(0))
+	//	{
+	//		mousePosOrigin = Input.mousePosition;
+	//		Debug.Log("Origin point for mouse set");
+	//	}
 
-		if (Input.GetMouseButton(0))
-		{
-			mouseCurrentPos = Input.mousePosition;
+	//	if (Input.GetMouseButton(0))
+	//	{
+	//		mouseCurrentPos = Input.mousePosition;
 
-			dragDisplacement = mouseCurrentPos - mousePosOrigin;
-			Debug.Log(dragDisplacement.y);
+	//		dragDisplacement = mouseCurrentPos - mousePosOrigin;
+	//		Debug.Log(dragDisplacement.y);
 
-			if (dragDisplacement.y > 0)
-			{
-				transform.position = new Vector3(transform.position.x, transform.position.y - dragDisplacement.y * Time.deltaTime, transform.position.z);
-			}
-			if (dragDisplacement.y < 0)
-				transform.position = new Vector3(transform.position.x, transform.position.y - dragDisplacement.y * Time.deltaTime, transform.position.z);
-		}
-	}
+	//		if (dragDisplacement.y > 0)
+	//		{
+	//			transform.position = new Vector3(transform.position.x, transform.position.y - dragDisplacement.y * Time.deltaTime, transform.position.z);
+	//		}
+	//		if (dragDisplacement.y < 0)
+	//			transform.position = new Vector3(transform.position.x, transform.position.y - dragDisplacement.y * Time.deltaTime, transform.position.z);
+	//	}
+	//}
 }
