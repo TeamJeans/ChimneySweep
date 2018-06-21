1979a1ecb0e9bd82309cdd39b4237054b3488218
AidanRMurray <AidanRMurray@users.noreply.github.com>
2018-06-21 16:24:01 +0100

    Started Tile generation

---
+++ ChimneySweep/Assets/Scripts/ChimneyTile.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class ChimneyTile : MonoBehaviour
+{
+	public string name;
+}
--- ChimneySweep/Assets/Scripts/TileManager.cs
+++ ChimneySweep/Assets/Scripts/TileManager.cs
@@ -1,23 +1,32 @@
 using UnityEngine;
 
-[System.Serializable]
-public class ChimneyTile
-{
-	public string name;
-}
-
 public class TileManager : MonoBehaviour {
 
 	[SerializeField]
 	ChimneyTile[] tiles;
 
+	public float spaceBetweenTiles = 100f;
+	Vector3 firstTilePos;
+	Vector3 tileSize;
+
+	bool tilesGenerated = false;
+
 	// Use this for initialization
 	void Start () {
-		
+		firstTilePos = tiles[0].transform.position;
+		tileSize = tiles[0].transform.localScale;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+		if (!tilesGenerated)
+		{
+			for (int i = 0; i < tiles.Length; i++)
+			{
+				tiles[i].transform.position = new Vector3(tiles[i].transform.position.x, tiles[i].transform.position.y + tiles[i].transform.localScale.y + spaceBetweenTiles, tiles[i].transform.position.z);
+			}
+		}
+
 	}
 }
