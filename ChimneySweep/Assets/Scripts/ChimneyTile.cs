aa025db949b03bf86a495c011f0fa90385855892
JackChateauLoney <32193286+JackChateauLoney@users.noreply.github.com>
2018-08-23 11:41:52 +0100

    Fixed scrolling
    can now scroll anywhere except current tile

--- ChimneySweep/Assets/Scripts/ChimneyTile.cs
+++ ChimneySweep/Assets/Scripts/ChimneyTile.cs
@@ -34,12 +34,16 @@ public class ChimneyTile : MonoBehaviour
 
 	void OnMouseOver()
 	{
-		// Check if the user has tapped
+		// Check if the user has their finger on the current tile, if so dont allow scroll up and down
 		if (Input.GetMouseButtonDown(0) && tileManager.CurrentlySelectedTile == gameObject)
 		{
 			selected = true;
 		}
-		if (!mouseOver)
+        if (Input.GetMouseButtonUp(0) && tileManager.CurrentlySelectedTile == gameObject)
+        {
+            selected = false;
+        }
+        if (!mouseOver)
 		{
 			mouseOver = true;
 		}
@@ -51,7 +55,7 @@ public class ChimneyTile : MonoBehaviour
 		{
 			selected = false;
 		}
-
+        
 
 		// Set the tile to have the information it needs
 		if (selected)
