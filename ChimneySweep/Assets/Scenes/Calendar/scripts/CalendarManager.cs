40087b318794e2eae504ea7dbe64269081a26ad0
JackChateauLoney <32193286+JackChateauLoney@users.noreply.github.com>
2018-08-26 17:01:39 +0100

    Started calendar

---
+++ ChimneySweep/Assets/Scenes/Calendar/scripts/CalendarManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;  // Required when Using UI elements.
+using UnityEngine;
+
+public class CalendarManager : MonoBehaviour {
+
+    public Text rentDue;
+    public int day;
+    public float dayCheck;
+    public int week;
+    public int rent;
+    public bool rentPaid;
+
+    // Use this for initialization
+    void Start()
+    {
+        //reset the check for rentPaid at start of scene
+        rentPaid = false;
+        //gets the current day and checks if end of week
+        day = StaticValueHolder.CurrentDay;
+        dayCheck = StaticValueHolder.CurrentDay / 7;
+        //used to increase rent
+        week = day / 7;
+        rent = 50 + week * 50;
+        //show how much rent is due at end of week
+        rentDue.text = "Rent Due: " + "\u00A3" + rent;
+    }
+
+
+	// Update is called once per frame
+	void Update ()
+    {
+        //if time to pay rent
+        if ((float)day / 7 == dayCheck && !rentPaid)
+        {
+            //if enough money to pay
+            if (StaticValueHolder.CurrentMoney >= rent)
+            {
+                StaticValueHolder.CurrentMoney -= rent;
+                rentPaid = true;
+                Debug.Log("Rent Paid: " + rentPaid);
+                
+            }
+            //if not enough to pay rent
+            else
+            {
+                Debug.Log("GameOver");
+            }
+        }
+    }
+}
---
+++ ChimneySweep/Assets/Scenes/Calendar/scripts/StaticValueHolder.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//holds values between scenes
+public static class StaticValueHolder
+{
+    //initialise here
+    private static int currentMoney, currentDay;
+
+    public static int CurrentMoney
+    {
+        get
+        {
+            return currentMoney;
+        }
+        set
+        {
+            currentMoney = value;
+        }
+    }
+
+    public static int CurrentDay
+    {
+        get
+        {
+            return currentDay;
+        }
+        set
+        {
+            currentDay = value;
+        }
+    }
+}
