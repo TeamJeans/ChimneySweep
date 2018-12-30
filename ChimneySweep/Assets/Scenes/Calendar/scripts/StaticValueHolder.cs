f8f35ccfb609966587e3cd269a9970b9f67f97f1
JackChateauLoney <32193286+JackChateauLoney@users.noreply.github.com>
2018-12-30 16:49:28 +0000

    day value arrays

--- ChimneySweep/Assets/Scenes/Calendar/scripts/StaticValueHolder.cs
+++ ChimneySweep/Assets/Scenes/Calendar/scripts/StaticValueHolder.cs
@@ -7,52 +7,37 @@ public static class StaticValueHolder
 {
     //initialise here
     private static int currentMoney, currentDay, currentWeek, totalMoney;
+    private static int[] dayValues = new int[7];
+
+
+    public static int[] DayValues
+    {
+        get { return dayValues; }
+        set { dayValues = value; }
+    }
+
 
     public static int CurrentMoney
     {
-        get
-        {
-            return currentMoney;
-        }
-        set
-        {
-            currentMoney = value;
-        }
+        get { return currentMoney; }
+        set { currentMoney = value; }
     }
 
     public static int CurrentDay
     {
-        get
-        {
-            return currentDay;
-        }
-        set
-        {
-            currentDay = value;
-        }
+        get { return currentDay; }
+        set { currentDay = value; }
     }
 
     public static int CurrentWeek
     {
-        get
-        {
-            return currentWeek;
-        }
-        set
-        {
-            currentWeek = value;
-        }
+        get { return currentWeek; }
+        set { currentWeek = value; }
     }
 
     public static int TotalMoney
     {
-        get
-        {
-            return totalMoney;
-        }
-        set
-        {
-            totalMoney = value;
-        }
+        get { return totalMoney; }
+        set { totalMoney = value; }
     }
 }
