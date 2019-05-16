5fba6e4c21adc394a198e7bd2e5aa759b8b57de7
JackChateauLoney <32193286+JackChateauLoney@users.noreply.github.com>
2019-05-16 17:17:07 +0100

    Buttons, but also emojis?

---
+++ ChimneySweep/Assets/Scenes/Calendar/scripts/dayOverDisplay.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class dayOverDisplay : MonoBehaviour {
+
+    public TextMeshProUGUI DayEnd;
+
+	// Use this for initialization
+	void Start ()
+    {
+        DayEnd.text = "End of day\n" + StaticValueHolder.CurrentDay;
+    }
+}
